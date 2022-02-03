d02034c0ebbf72f15b5a376a65ff1b03e1e06451
agiannell-ts <anthonygiannell@truckstop.com>
2022-02-03 00:05:57 -0700

    feat: update session endpoint to accept custom donation

--- pages/api/checkout/session.ts
+++ pages/api/checkout/session.ts
@@ -1,32 +1,47 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import Stripe from 'stripe';
+import { formatAmountForStripe } from "utils/stripe-helpers";
+import { CURRENCY, MIN_AMOUNT, MAX_AMOUNT } from "config";
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2020-08-27',
-  typescript: true
+  apiVersion: "2020-08-27",
+  typescript: true,
 });
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { price } = req.body;
-  if (req.method === 'POST') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const amount: number = req.body.amount;
     try {
-      const session = await stripe.checkout.sessions.create({
+      if (!(amount >= MIN_AMOUNT && amount <= MAX_AMOUNT)) {
+        throw new Error("Invalid amount.");
+      }
+      const params: Stripe.Checkout.SessionCreateParams = {
+        submit_type: "donate",
         line_items: [
           {
-            price,
-            quantity: 1
-          }
+            name: "Donate to Amplify Hope",
+            amount: formatAmountForStripe(amount, CURRENCY),
+            currency: CURRENCY,
+            quantity: 1,
+          },
         ],
-        mode: 'payment',
-        success_url: `${ req.headers.origin }/?success=true`,
-        cancel_url: `${ req.headers.origin }/?canceled=true`
-      });
-      res.redirect(303, session.url!);
-    } catch (err: any) {
-      res.status(err.statusCode || 500).json(err.message)
+        success_url: `${req.headers.origin}/?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+
+      const checkoutSession: Stripe.Checkout.Session =
+        await stripe.checkout.sessions.create(params);
+      res.status(200).json(checkoutSession);
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : "Internal server error";
+      res.status(500).json({ statusCode: 500, message: errorMessage });
     }
   } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed')
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
 }
