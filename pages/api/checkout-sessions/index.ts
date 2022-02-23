543d09ef2ef41920e139aacb4076cdc7fe7e23c4
agiannell-ts <anthonygiannell@truckstop.com>
2022-02-23 14:48:46 -0700

    feat: create subdirectory to add get request based on session ID

--- pages/api/checkout-sessions/index.ts
+++ pages/api/checkout-sessions/index.ts
@@ -28,8 +28,8 @@ export default async function handler(
             quantity: 1
           }
         ],
-        success_url: `${req.headers.origin}/?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${req.headers.origin}/?canceled=true`
+        success_url: `${req.headers.origin}/result/{CHECKOUT_SESSION_ID}`,
+        cancel_url: req.headers.origin as string
       };
 
       const checkoutSession: Stripe.Checkout.Session =
