2407db3ede4874c0ca62a3c0dc9a9f82e5082cf1
agiannell-ts <anthonygiannell@truckstop.com>
2022-02-28 16:47:18 -0700

    feat: strip down result page for future checkout refactor

--- pages/result/[id].tsx
+++ pages/result/[id].tsx
@@ -1,48 +1,11 @@
-import { useEffect, useState } from 'react';
-import { GetServerSideProps } from 'next';
-import { prisma } from 'common/database';
-import fetchJson from 'utils/fetchJson';
+import React from 'react';
 
-const Result = props => {
-  const [session, setSession] = useState({});
-  const { id } = props;
-
-  const getSession = async (session_id: string) => {
-    const foundSession = await fetchJson(
-      `/api/checkout-sessions/${session_id}`,
-      {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
-      }
-    );
-    setSession(foundSession);
-  };
-
-  useEffect(() => {
-    getSession(id);
-  }, []);
-
-  useEffect(() => {
-    if (Object.keys(session).length !== 0) {
-    }
-  }, [session]);
-
-  console.log('client session: ', session);
+const Result = () => {
   return (
     <div>
       <div>Result page</div>
     </div>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async context => {
-  const { id } = context.query;
-
-  return {
-    props: {
-      id
-    }
-  };
 };
 
 export default Result;
