4ffa32f0943c27f91876d2d7d01dbb0290c940b6
agiannell-ts <anthonygiannell@truckstop.com>
2022-03-11 10:55:15 -0700

    feat: add form validation schema

---
+++ utils/validation-schema.ts
@@ -0,0 +1,22 @@
+import * as yup from 'yup';
+
+export const OneTimeDonationSchema = yup
+  .object({
+    email: yup
+      .string()
+      .email('Please enter a valid email address')
+      .required('Required'),
+    amount: yup.number().required()
+  })
+  .required();
+
+export const RecurringDonationSchema = yup
+  .object({
+    email: yup
+      .string()
+      .email('Please enter a valid email address')
+      .required('Required'),
+    amount: yup.number().required(),
+    interval: yup.string().required()
+  })
+  .required();
