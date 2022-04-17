40a7b831d2892e07f19f188b5dc96d32de0beeb1
agiannell-ts <anthonygiannell@truckstop.com>
2022-04-17 14:49:20 -0600

    feat: 1. incorporate interval into form 2. position button at bottom of form 3. bring donation input into formik as TextField

--- components/Checkout/CheckoutForm.tsx
+++ components/Checkout/CheckoutForm.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useCallback } from 'react';
 import { Formik, Form, FormikHelpers } from 'formik';
 import { Button } from '@react-md/button';
-import { Card, CardContent } from '@react-md/card';
+import { Card, CardContent, CardActions } from '@react-md/card';
 import { TextField, Select, FormMessage, ListboxOption } from '@react-md/form';
-import { EmailSVGIcon } from '@react-md/material-icons';
+import { EmailSVGIcon, AttachMoneySVGIcon } from '@react-md/material-icons';
 import { Grid, GridCell } from '@react-md/utils';
 import Stripe from 'stripe';
-import { CustomDonationInput, Loading, Container } from 'components';
+import { Loading } from 'components';
 import { formatAmountForDisplay } from 'utils/stripe-helpers';
 import getStripe from 'utils/get-stripejs';
 import fetchJson from 'utils/fetchJson';
@@ -17,139 +17,175 @@ import {
 import { DonationRequestBody } from 'common/types';
 import * as config from '../../config';
 
+import styles from './CheckoutForm.module.scss';
+
 type CheckoutFormProps = {
   isRecurring: boolean;
 };
 
+enum IntervalOptions {
+  month = 'month',
+  quarter = 'quarter',
+  year = 'year'
+}
+
 type FormValues = {
   email: string;
   amount: string;
-  interval: Stripe.Price.Recurring.Interval;
+  interval: IntervalOptions;
 };
 
-const intervalOptions = ['week', 'month', 'year'];
+const intervalOptions = [
+  IntervalOptions.month,
+  IntervalOptions.quarter,
+  IntervalOptions.year
+];
 
 export const CheckoutForm = (props: CheckoutFormProps) => {
   const { isRecurring } = props;
   const [loading, setLoading] = useState<boolean>(false);
-  const [interval, setInterval] =
-    useState<Stripe.Price.Recurring.Interval>('month');
+  const [intervalCount, setIntervalCount] = useState<number>(1);
+  const [interval, setInterval] = useState<IntervalOptions>(
+    IntervalOptions.month
+  );
 
   const handleChange = useCallback(
-    (nextValue: Stripe.Price.Recurring.Interval, _option: ListboxOption) => {
+    (nextValue: IntervalOptions, _option: ListboxOption) => {
       setInterval(nextValue);
+      if (nextValue === IntervalOptions.quarter) {
+        setIntervalCount(3);
+      }
     },
     []
   );
 
   return (
-    <Container>
-      <Grid>
-        <GridCell colSpan={12}>
-          <Card>
-            <CardContent>
-              <Formik
-                enableReinitialize={true}
-                initialValues={{
-                  email: '',
-                  amount: Math.round(config.MIN_AMOUNT).toFixed(2).toString(),
-                  interval
-                }}
-                validationSchema={
-                  isRecurring ? RecurringDonationSchema : OneTimeDonationSchema
+    <Grid>
+      <GridCell colSpan={12}>
+        <Card className={styles.checkoutFormCard}>
+          <Formik
+            enableReinitialize={true}
+            initialValues={{
+              email: '',
+              amount: Math.round(config.MIN_AMOUNT).toFixed(2).toString(),
+              interval
+            }}
+            validationSchema={
+              isRecurring ? RecurringDonationSchema : OneTimeDonationSchema
+            }
+            onSubmit={async (
+              formValues: FormValues,
+              { setSubmitting }: FormikHelpers<FormValues>
+            ) => {
+              setLoading(true);
+              const amount: number = +formValues.amount;
+              const stripeInterval: Stripe.Price.Recurring.Interval =
+                formValues.interval === IntervalOptions.year ? 'year' : 'month';
+              const requestBody: DonationRequestBody = {
+                email: formValues.email,
+                amount,
+                interval: isRecurring ? stripeInterval : undefined,
+                interval_count: intervalCount
+              };
+
+              const response = await fetchJson(
+                `/api/checkouts/${isRecurring ? 'recurring' : 'one-time'}`,
+                {
+                  method: 'POST',
+                  headers: { 'Content-Type': 'application/json' },
+                  body: JSON.stringify(requestBody)
                 }
-                onSubmit={async (
-                  formValues: FormValues,
-                  { setSubmitting }: FormikHelpers<FormValues>
-                ) => {
-                  setLoading(true);
-                  const amount: number = +formValues.amount;
-                  const requestBody: DonationRequestBody = {
-                    email: formValues.email,
-                    amount,
-                    interval: isRecurring ? formValues.interval : undefined
-                  };
+              );
 
-                  const response = await fetchJson(
-                    `/api/checkouts/${isRecurring ? 'recurring' : 'one-time'}`,
-                    {
-                      method: 'POST',
-                      headers: { 'Content-Type': 'application/json' },
-                      body: JSON.stringify(requestBody)
-                    }
-                  );
-
-                  const stripe = await getStripe();
-                  const { error } = await stripe!.redirectToCheckout({
-                    sessionId: response.id
-                  });
-                  console.warn(error.message);
-                  setLoading(false);
-                  setSubmitting(false);
-                }}
-              >
-                {props => {
-                  return (
-                    <Form>
-                      <TextField
-                        id="email"
-                        value={props.values.email}
-                        onChange={props.handleChange}
-                        theme="outline"
-                        type="email"
-                        rightChildren={<EmailSVGIcon />}
-                        label="Email"
-                        name="email"
-                        placeholder="user@example.com"
-                        error={Boolean(props.errors.email)}
-                        // style={{ marginBottom: '2rem' }}
-                      />
-                      <FormMessage id="email-message" error>
-                        {props.errors.email}
-                      </FormMessage>
-                      <CustomDonationInput
-                        name="customDonation"
-                        value={props.values.amount}
-                        min={config.MIN_AMOUNT}
-                        max={config.MAX_AMOUNT}
-                        currency={config.CURRENCY}
-                        onChange={props.handleChange}
-                      />
-                      {isRecurring && (
-                        <Select
-                          id="interval"
-                          options={intervalOptions}
-                          value={props.values.interval}
-                          onChange={handleChange}
+              const stripe = await getStripe();
+              const { error } = await stripe!.redirectToCheckout({
+                sessionId: response.id
+              });
+              console.warn(error.message);
+              setLoading(false);
+              setSubmitting(false);
+            }}
+          >
+            {props => {
+              return (
+                <Form className={styles.checkoutForm}>
+                  <CardContent>
+                    <Grid>
+                      <GridCell colSpan={12}>
+                        <TextField
+                          id="email"
+                          value={props.values.email}
+                          onChange={props.handleChange}
+                          theme="outline"
+                          type="email"
+                          rightChildren={<EmailSVGIcon />}
+                          label="Email"
+                          name="email"
+                          placeholder="user@example.com"
+                          error={Boolean(props.errors.email)}
                         />
-                      )}
-                      <Button
-                        type="submit"
-                        style={{ marginTop: '2rem', width: '100%' }}
-                        disabled={loading}
-                        theme="primary"
-                        themeType="outline"
-                      >
-                        {!loading ? (
-                          <>
-                            Donate{' '}
-                            {formatAmountForDisplay(
-                              +props.values.amount,
-                              config.CURRENCY
-                            )}{' '}
-                          </>
-                        ) : (
-                          <Loading />
+                        <FormMessage id="email-message" error>
+                          {props.errors.email}
+                        </FormMessage>
+                      </GridCell>
+                      <GridCell colSpan={!isRecurring ? 12 : 6}>
+                        <TextField
+                          id="donation"
+                          theme="outline"
+                          leftChildren={<AttachMoneySVGIcon />}
+                          value={props.values.amount}
+                          label="Amount"
+                          name="amount"
+                          placeholder={config.MIN_AMOUNT.toString()}
+                          // type="number"
+                          min={config.MIN_AMOUNT}
+                          max={config.MAX_AMOUNT}
+                          onChange={props.handleChange}
+                          error={Boolean(props.errors.amount)}
+                        />
+                        <FormMessage id="amount-message" error>
+                          {props.errors.amount}
+                        </FormMessage>
+                      </GridCell>
+                      <GridCell colSpan={6}>
+                        {isRecurring && (
+                          <Select
+                            id="interval"
+                            options={intervalOptions}
+                            value={props.values.interval}
+                            onChange={handleChange}
+                          />
                         )}
-                      </Button>
-                    </Form>
-                  );
-                }}
-              </Formik>
-            </CardContent>
-          </Card>
-        </GridCell>
-      </Grid>
-    </Container>
+                      </GridCell>
+                    </Grid>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      type="submit"
+                      style={{ marginTop: '2rem', width: '100%' }}
+                      disabled={loading}
+                      theme="primary"
+                      themeType="outline"
+                    >
+                      {!loading ? (
+                        <>
+                          Donate{' '}
+                          {formatAmountForDisplay(
+                            +props.values.amount,
+                            config.CURRENCY
+                          )}{' '}
+                        </>
+                      ) : (
+                        <Loading />
+                      )}
+                    </Button>
+                  </CardActions>
+                </Form>
+              );
+            }}
+          </Formik>
+        </Card>
+      </GridCell>
+    </Grid>
   );
 };
