e4a1a3d53ea7cc9cfd149b55ea25b81d74a31243
agiannell-ts <anthonygiannell@truckstop.com>
2022-04-17 14:45:40 -0600

    feat: 1. add tabs to toggle one-time and recurring 2. remove app classname to accommodate tabs layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,21 @@
 import type { NextPage } from 'next';
 import { CheckoutForm, Container } from 'components';
 import { TabsManager, Tabs, TabPanels, TabPanel } from '@react-md/tabs';
+import styles from './index.module.scss';
 
 const tabs = ['One Time Donation', 'Recurring Donation'];
 
 const Home: NextPage = () => {
   return (
-    <Container style={{ display: 'flex', flexDirection: 'column' }}>
+    <Container className={styles.indexContainer}>
       <img
         src="/images/logo-linear.svg"
         alt="Amplify Hope"
         style={{ height: '4rem' }}
       />
       <TabsManager tabs={tabs} tabsId="donation-type">
-        <Tabs />
-        <TabPanels>
+        <Tabs className={styles.indexTabs} />
+        <TabPanels style={{ width: '100%' }}>
           <TabPanel>
             <CheckoutForm isRecurring={false} />
           </TabPanel>
