7d3d54a6e24a634b90325dadb200ce03e887661e
Anthony Giannell <anthony-giannell@pluralsight.com>
2022-10-19 21:56:01 -0600

    added dockerfile and modified next config per next docs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY
-  }
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  output: 'standalone'
+}
