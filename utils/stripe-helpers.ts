20bf41041e0503355a9cef2258f4e306fdd20dd2
Anthony Giannell <anthony-giannell@pluralsight.com>
2024-04-28 15:58:29 -0600

    added helper function to calculate stripe fees

--- utils/stripe-helpers.ts
+++ utils/stripe-helpers.ts
@@ -1,3 +1,5 @@
+import { STRIPE_FEE_PERCENT_MODIFIER, STRIPE_FEE_FIXED } from '../config'
+
 export const formatAmountForDisplay = (
   amount: number,
   currency: string
@@ -20,4 +22,10 @@ export const formatAmountForDisplayFromStripe = (
     currencyDisplay: 'symbol'
   })
   return numberFormat.format(amount / 100)
+}
+
+export const calculateStripeFees = (amount: number): number => {
+  return Number(
+    (amount * STRIPE_FEE_PERCENT_MODIFIER + STRIPE_FEE_FIXED).toFixed(2)
+  )
 }
