db149b90637ba5e7f44967b1e7fc660cc710c02c
Anthony Giannell <anthony-giannell@pluralsight.com>
2025-01-20 08:58:26 -0700

    added suspense

--- app/auth/verify/page.tsx
+++ app/auth/verify/page.tsx
@@ -2,28 +2,31 @@
 
 import { Footer } from 'components'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 const Verify = () => {
   const searchParams = useSearchParams()
   const email = searchParams?.get('email')
 
   return (
-    <div className='text-white flex flex-col items-center w-full h-screen bg-ahBlue'>
-      <div className='lg:mt-20 mt-40 mb-20 lg:mb-40 flex flex-col items-center justify-center w-5/6 h-full md:w-2/3 lg:w-1/3'>
-        <p className='text-4xl font-bold font-subheading'>Check Your Email</p>
-        <p className='text-lg my-8'>
-          We&lsquo;ve sent a magic link to{' '}
-          <span className='font-bold'>{email}</span>. The link will expire, so
-          please click on it soon.
-        </p>
-        <p className='text-sm'>
-          Can&lsquo;t find your magic link? Check your spam folder!
-        </p>
+    <Suspense>
+      <div className='text-white flex flex-col items-center w-full h-screen bg-ahBlue'>
+        <div className='lg:mt-20 mt-40 mb-20 lg:mb-40 flex flex-col items-center justify-center w-5/6 h-full md:w-2/3 lg:w-1/3'>
+          <p className='text-4xl font-bold font-subheading'>Check Your Email</p>
+          <p className='text-lg my-8'>
+            We&lsquo;ve sent a magic link to{' '}
+            <span className='font-bold'>{email}</span>. The link will expire, so
+            please click on it soon.
+          </p>
+          <p className='text-sm'>
+            Can&lsquo;t find your magic link? Check your spam folder!
+          </p>
+        </div>
+        <div className='lg:fixed bottom-0 w-full'>
+          <Footer />
+        </div>
       </div>
-      <div className='lg:fixed bottom-0 w-full'>
-        <Footer />
-      </div>
-    </div>
+    </Suspense>
   )
 }
 
